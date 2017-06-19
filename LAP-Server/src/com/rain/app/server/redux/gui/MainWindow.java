78b73c64192b2dddb5348b29a996c5462dff3147
Ryan May <xtheraingoddessx@gmail.com>
2017-06-18 19:54:22 -0700

    added menubar to gui

--- LAP-Server/src/com/rain/app/server/redux/gui/MainWindow.java
+++ LAP-Server/src/com/rain/app/server/redux/gui/MainWindow.java
@@ -4,8 +4,17 @@
 package com.rain.app.server.redux.gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -32,10 +41,97 @@ public class MainWindow extends Application {
 	@Override
 	public void start(Stage root) throws Exception {
 		AnchorPane mainComponentPane = new AnchorPane();
+		
 		Scene mainScene = new Scene(mainComponentPane, WINDOW_MIN_WIDTH, WINDOW_MIN_HEIGHT);
+		
 		root.setScene(mainScene);
 		root.show();
 		
+	}
+	
+	private AnchorPane getMainComponentPane(){
+		AnchorPane mainComponentPane = new AnchorPane();
+		mainComponentPane.setMinWidth(WINDOW_MIN_WIDTH);
+		mainComponentPane.setMinHeight(WINDOW_MIN_HEIGHT);
+		mainComponentPane.getChildren().add(getMainVerticalBox());
+		return mainComponentPane;
+	}
+	
+	private VBox getMainVerticalBox(){
+		VBox vbox = new VBox();
+		vbox.getChildren().addAll(getMenuBar(), getMainBorderPane());
+		return vbox;
+	}
+	
+//MenuBar
+	private MenuBar getMenuBar(){
+		MenuBar menuBar = new MenuBar();
+        
+        // --- Menu File
+        Menu menuFile = getMenuFileOption();
+        
+        // --- Menu Edit
+        Menu menuEdit = getMenuEditOption();
+ 
+        // --- Menu View
+        Menu menuView = getMenuViewOption();
+        
+        menuBar.setMinWidth(WINDOW_MIN_WIDTH);
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+        menuBar.autosize();
+        return menuBar;
+	}
+	
+	private Menu getMenuFileOption(){
+		Menu menu = new Menu("File");
+		
+		MenuItem add = getMenuItem("Add", new MenuAction(){
+			@Override
+			public void execute() {
+				
+			}
+		});
+		
+		MenuItem remove = getMenuItem("Remove", new MenuAction(){
+			@Override
+			public void execute() {
+				
+			}
+		});
+		
+	    menu.getItems().addAll(add, remove);
+	    return menu;
+	}
+	
+	private MenuItem getMenuItem(String name, MenuAction action){
+		MenuItem item = new MenuItem(name,
+            new ImageView(new Image("res/" + name.toLowerCase()+".png")));
+        item.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+			public void handle(ActionEvent t) {
+                action.execute();
+            }
+        });
+        return item;
+	}
+	
+	private Menu getMenuEditOption(){
+		Menu menu = new Menu("Edit");
+		return menu;
+	}
+	
+	private Menu getMenuViewOption(){
+		Menu menu = new Menu("View");
+		return menu;
+	}
+	
+	//Main border Pane
+	private BorderPane getMainBorderPane(){
+		BorderPane mainBorderPane = new BorderPane();
+		//mainBorderPane.setTop(getArchiveControls());
+		//mainBorderPane.setRight(getArchiveData());
+		//mainBorderPane.setLeft(getArchiveFileSystem());
+		return mainBorderPane;
 	}
 
 }
