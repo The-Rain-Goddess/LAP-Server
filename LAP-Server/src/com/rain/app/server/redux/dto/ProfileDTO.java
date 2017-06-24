cc237f436dd7d929ae4739e0970f82cf23ed76b1
Ryan May <xtheraingoddessx@gmail.com>
2017-06-23 18:34:45 -0700

    initial commit

---
+++ LAP-Server/src/com/rain/app/server/redux/dto/ProfileDTO.java
@@ -0,0 +1,11 @@
+package com.rain.app.server.redux.dto;
+
+import java.io.Serializable;
+
+public class ProfileDTO implements Serializable {
+
+	public ProfileDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
