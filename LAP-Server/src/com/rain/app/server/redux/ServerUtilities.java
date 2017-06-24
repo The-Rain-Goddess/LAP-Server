2ccbb53cc90b5ea07a9803a8a26ce359ee8d70dd
Ryan May <xtheraingoddessx@gmail.com>
2017-06-23 17:44:54 -0700

    updated method sigs to match SummonerData

--- LAP-Server/src/com/rain/app/server/redux/ServerUtilities.java
+++ LAP-Server/src/com/rain/app/server/redux/ServerUtilities.java
@@ -28,9 +28,9 @@ public class ServerUtilities {
 		} return list;
 	}
 	
-	public static String mapOfListsToString(Map<Integer, List<AggregatedChampionData>> rankedChampionDataMap){
+	public static String mapOfListsToString(Map<Integer, ArrayList<AggregatedChampionData>> rankedChampionDataMap){
 		String r = "";
-		for(Entry<Integer, List<AggregatedChampionData>> entry : rankedChampionDataMap.entrySet()){
+		for(Entry<Integer, ArrayList<AggregatedChampionData>> entry : rankedChampionDataMap.entrySet()){
 			r = r + entry.getKey() + ":\n";
 			for(Object s : entry.getValue()){
 				r = r + s.toString() + "\n";
