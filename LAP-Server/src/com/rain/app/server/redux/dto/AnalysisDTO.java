6e46eff0b994779609f436f2044fc4f627c5bba7
Ryan May <xtheraingoddessx@gmail.com>
2017-06-23 18:34:32 -0700

    inital commit

---
+++ LAP-Server/src/com/rain/app/server/redux/dto/AnalysisDTO.java
@@ -0,0 +1,11 @@
+package com.rain.app.server.redux.dto;
+
+import java.io.Serializable;
+
+public class AnalysisDTO implements Serializable {
+
+	public AnalysisDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
