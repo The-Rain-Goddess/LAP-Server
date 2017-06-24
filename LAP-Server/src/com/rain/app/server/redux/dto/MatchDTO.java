40c76e529b78b518030955565a123ff170b4e064
Ryan May <xtheraingoddessx@gmail.com>
2017-06-23 18:35:02 -0700

    intial commit

---
+++ LAP-Server/src/com/rain/app/server/redux/dto/MatchDTO.java
@@ -0,0 +1,11 @@
+package com.rain.app.server.redux.dto;
+
+import java.io.Serializable;
+
+public class MatchDTO implements Serializable {
+
+	public MatchDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
