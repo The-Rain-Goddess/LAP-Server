bba878adf76235602e4d571074b288beaff9172b
Ryan May <xtheraingoddessx@gmail.com>
2017-06-23 19:25:29 -0700

    added instance variables and getter/setters

--- LAP-Server/src/com/rain/app/server/redux/dto/AnalysisDTO.java
+++ LAP-Server/src/com/rain/app/server/redux/dto/AnalysisDTO.java
@@ -1,6 +1,10 @@
 package com.rain.app.server.redux.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import com.rain.app.server.redux.AggregatedChampionData;
 
 public class AnalysisDTO implements Serializable {
 
@@ -8,9 +12,24 @@ public class AnalysisDTO implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 4882364436013608070L;
+	private TreeMap<Integer, ArrayList<AggregatedChampionData>> championData;
 
-	public AnalysisDTO() {
-		// TODO Auto-generated constructor stub
+	public AnalysisDTO(TreeMap<Integer, ArrayList<AggregatedChampionData>> championData) {
+		this.championData = championData;
+	}
+
+	/**
+	 * @return the championData
+	 */
+	public TreeMap<Integer, ArrayList<AggregatedChampionData>> getChampionData() {
+		return championData;
+	}
+
+	/**
+	 * @param championData the championData to set
+	 */
+	public void setChampionData(TreeMap<Integer, ArrayList<AggregatedChampionData>> championData) {
+		this.championData = championData;
 	}
 
 }
