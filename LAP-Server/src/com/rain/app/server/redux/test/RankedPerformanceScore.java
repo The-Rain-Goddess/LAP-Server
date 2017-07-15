5c0feae83503525addacacce6a7f200305c9f741
Ryan May <xtheraingoddessx@gmail.com>
2017-07-15 12:50:43 -0700

    intial commit for RankedPerformanceScore

---
+++ LAP-Server/src/com/rain/app/server/redux/test/RankedPerformanceScore.java
@@ -0,0 +1,11 @@
+package com.rain.app.server.redux.test;
+
+import com.rain.app.service.riot.api.endpoints.match.dto.Participant;
+
+public class RankedPerformanceScore {
+
+	public RankedPerformanceScore(Participant p) {
+		
+	}
+
+}
