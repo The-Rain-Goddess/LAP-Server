a73e3148e5a837394a640ed339e677b41c52f362
Ryan May <xtheraingoddessx@gmail.com>
2017-09-02 17:14:15 -0700

    added the member field 'score' and a/m for it

--- LAP-Server/src/com/rain/app/server/redux/test/RankedPerformanceScore.java
+++ LAP-Server/src/com/rain/app/server/redux/test/RankedPerformanceScore.java
@@ -3,9 +3,23 @@ package com.rain.app.server.redux.test;
 import com.rain.app.service.riot.api.endpoints.match.dto.Participant;
 
 public class RankedPerformanceScore {
+	
+	private long score;
 
 	public RankedPerformanceScore(Participant p) {
-		
+		score = 0; // = 
+		calculateRankedPerformanceScore(p);
+	}
+	
+	
+//private a/m	
+	private long calculateRankedPerformanceScore(Participant p){
+		return 0;
+	}
+
+//non-private a/m	
+	public long getValue(){
+		return score;
 	}
 
 }
